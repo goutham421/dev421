b8d27a993fcfad9788c604559f426e83ad49205b
root <root@ip-172-31-76-149.ec2.internal>
2026-03-31 06:32:22 +0000

    new java code added

--- app.java
+++ app.java
@@ -17,3 +17,14 @@ public class MultiplyTwoNumbers {
         System.out.println("The product is: " + product);
     }
 }
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int number = 20;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
