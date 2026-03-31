f659528c24955f4dc7860c831def8f6d555bc545
goutham421 <saigoutham421@gmail.com>
2026-03-31 16:26:10 +0530

    code has reduced

--- app.java
+++ app.java
@@ -5,46 +5,4 @@ public class HelloWorld {
 }
 
 
-public class MultiplyTwoNumbers {
 
-    public static void main(String[] args) {
-
-        float first = 1.5f;
-        float second = 2.0f;
-
-        float product = first * second;
-
-        System.out.println("The product is: " + product);
-    }
-}
-public class EvenOddCheck {
-    public static void main(String[] args) {
-        int number = 20;
-
-        if (number % 2 == 0) {
-            System.out.println(number + " is an even number.");
-        } else {
-            System.out.println(number + " is an odd number.");
-        }
-    }
-}
-def is_palindrome(s):
-    return s == s[::-1]
-print(is_palindrome("madam")) # Output: True
-public class VariablesExample {
-    public static void main(String[] args) {
-        // Declare and initialize variables of different data types
-        int number = 10;
-        double decimal = 99.99;
-        boolean isTrue = true;
-        String text = "Java Snippets";
-        char character = 'A';
-
-        // Print the values
-        System.out.println("Integer variable: " + number);
-        System.out.println("Double variable: " + decimal);
-        System.out.println("Boolean variable: " + isTrue);
-        System.out.println("String variable: " + text);
-        System.out.println("Character variable: " + character);
-    }
-}
