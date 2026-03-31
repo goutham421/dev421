55b236eeda358b6f15ecc7fc4c2f461c1c5ae2d7
goutham421 <saigoutham421@gmail.com>
2026-03-31 10:51:41 +0530

    new python code added

--- class.py
+++ class.py
@@ -2,3 +2,9 @@ num1 = 10
 num2 = 20
 sum_result = num1 + num2
 print(f"The sum of {num1} and {num2} is {sum_result}")
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
